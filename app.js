befe719b0506dfcc7bb5d33b7e33beae9d9fb5b5
Jacob Corum <cor15031@byui.edu>
2020-06-23 16:39:16 -0600

    added a question and added a .gitignore, and downloaded inquirer

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js")
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js")
 
-fs.writeFile("index.html", generatePage("Jane", "janehub"), err => {
-    if (err) throw err;
-    console.log("Portfolio Complete! Check out index.html to see the output!");
+// fs.writeFile("index.html", generatePage("Jane", "janehub"), err => {
+//     if (err) throw err;
+//     console.log("Portfolio Complete! Check out index.html to see the output!");
     
-})
+// })
   
+const inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+        {
+            type: "input", 
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
